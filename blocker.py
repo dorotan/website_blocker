f62457238bdfbc1c7d0bdfdd960827a5d72b855c
Dorota Niezborała <d.niezborala@merixstudio.com>
2017-09-22 19:14:49 +0200

    Add requirements $ a few variables

---
+++ blocker.py
@@ -0,0 +1,8 @@
+import time
+hosts_path =r"C:\Windows\System32\drivers\etc\hosts"
+redirect="127.0.0.1"
+website_list = ["www.facebook.com","facebook.com", "gmail.com", "wp.pl", "www.wp.pl"]
+
+while True:
+    print(1)
+    time.sleep(5)
