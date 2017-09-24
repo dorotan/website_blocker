62cfd35756759f9ca61d80f2a30bd9d4033b7578
Dorota Niezborała <d.niezborala@merixstudio.com>
2017-09-24 20:15:33 +0200

    Add changes to hosts temp file file
    - add func checking if website exsits
    - add func defining working & fun hours
    - read & write in hosts temp file

--- blocker.py
+++ blocker.py
@@ -1,8 +1,28 @@
 import time
+from datetime import datetime as mytime
+
+hosts_temp="hosts"
 hosts_path =r"C:\Windows\System32\drivers\etc\hosts"
 redirect="127.0.0.1"
 website_list = ["www.facebook.com","facebook.com", "gmail.com", "wp.pl", "www.wp.pl"]
 
 while True:
-    print(1)
+    if mytime(mytime.now().year, mytime.now().month, mytime.now().day,16) < mytime.now() < mytime(mytime.now().year, mytime.now().month, mytime.now().day,22):
+        print("Working hours")
+        with open(hosts_temp, 'r+') as file:
+            content=file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect+" "+website+ "\n\t")
+    else:
+        with open(hosts_temp, "r+") as fiel:
+            content=file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
+        print("Fun python hours...")
     time.sleep(5)
